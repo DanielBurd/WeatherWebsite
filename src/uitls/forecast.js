62bd497d47c0bce12c229f0cf1088bb378195f84
DanielBurd <danielb18@gmail.com>
2020-06-11 20:52:35 +0300

    Wind Speed Was added to Weather String

--- src/uitls/forecast.js
+++ src/uitls/forecast.js
@@ -2,15 +2,16 @@ const request=require('request');
 
 
 forecast=(lat,long,callback)=>{
-    const data=`The coordients for ypu loaction are ${lat}, ${long}`;
     const url=`http://api.weatherstack.com/current?access_key=02caa714fef2f242d2d2c5525e737531&query=${lat},${long}&units=m`;
+    
     request({url,json:true},(error,{body})=>{
         if(error){
              callback('unable to connect to weather service',undefined);
         }else if(body.error){
             callback('unable to find locaion',undefined)
         }else{
-            callback(undefined,data+'<br>, '+ body.current.weather_descriptions[0]+', It is '+body.current.temperature +' degrees out, It feels like '+body.current.feelslike+' degrees out.');
+            
+            callback(undefined, body.current.weather_descriptions[0]+', It is '+body.current.temperature +' degrees out, It feels like '+body.current.feelslike+' degrees out. The currnet wind speed is '+body.current.wind_speed);
         }
     });
     
